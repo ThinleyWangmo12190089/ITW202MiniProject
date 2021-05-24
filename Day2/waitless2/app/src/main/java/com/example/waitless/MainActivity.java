fc192b2c35af7dcc7f99e45f1c5462498dbce49b
Thinley Wangmo <12190089.gcit@rub.edu.bt>
2021-05-24 21:22:02 +0530

    day2

---
+++ Day2/waitless2/app/src/main/java/com/example/waitless/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.waitless;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText emailEt,passwordEt;
+    private Button SignInButton;
+    private TextView SignUpTv;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailEt = findViewById(R.id.email);
+        passwordEt = findViewById(R.id.password);
+        SignInButton = findViewById(R.id.login);
+        progressDialog = new ProgressDialog(this);
+        SignUpTv = findViewById(R.id.signUpTv);
+        SignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+        SignUpTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+    private void Login() {
+            String email=emailEt.getText().toString();
+            String password=passwordEt.getText().toString();
+            if(TextUtils.isEmpty(email)){
+                emailEt.setError("Enter your email");
+                return;
+            }
+            else if(TextUtils.isEmpty(password)){
+                passwordEt.setError("Enter your password");
+                return;
+            }
+
+            progressDialog.setMessage("Please wait");
+            progressDialog.show();
+            progressDialog.setCanceledOnTouchOutside(false);
+            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this,DashboardActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Sign in fail!", Toast.LENGTH_LONG).show();
+                    }
+                    progressDialog.dismiss();
+
+                }
+            });
+    }
+}
